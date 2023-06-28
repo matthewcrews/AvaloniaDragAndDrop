c3fc4893093ff7cda127aa40da8584ca71a99c42
Matthew Crews <matthew@crews.email>
2023-06-28 11:19:55 -0700

    multi button

--- Program.fs
+++ Program.fs
@@ -9,47 +9,70 @@ module Counter =
     open Avalonia.Layout
 
     type State = {
-        IsSelected: bool
-        X : float
-        Y : float
+        SelectedButton: int
+        ButtonLocations: Point[]
+        PointerLocation: Point
     }
     let init() = {
-        IsSelected = false
-        X = 10.0
-        Y = 10.0
+        SelectedButton = -1
+        ButtonLocations = 
+            [|
+                Point (10.0, 10.0)
+                Point (10.0, 100.0)
+            |]
+        PointerLocation = Point (0.0, 0.0)
     }
 
+    [<RequireQualifiedAccess>]
     type Msg =
-    | Selected
+    | Selected of buttonIdx: int * pointerLocation: Point
     | Deselected
-    | Move of x: float * y: float
+    | Move of buttonIdx: int * newPointerPoint: Point
 
     let update (msg: Msg) (state: State) : State =
         match msg with
-        | Selected ->
-            { state with IsSelected = true }
-        | Deselected ->
-            { state with IsSelected = false }
-        | Move (x, y) ->
-            if state.IsSelected then
-                { state with X = x; Y = y }
+        | Msg.Selected (buttonIdx, position) ->
+            { state with
+                SelectedButton = buttonIdx
+                PointerLocation = position }
+
+        | Msg.Deselected ->
+            { state with
+                SelectedButton = -1 }
+
+        | Msg.Move (buttonIdx, newPointerPoint) ->
+            if state.SelectedButton = buttonIdx then
+                let delta = newPointerPoint - state.PointerLocation
+                let newState =
+                    { state with
+                        PointerLocation = newPointerPoint }
+                newState.ButtonLocations[buttonIdx] <- state.ButtonLocations[buttonIdx] + delta
+                newState
             else
                 state
 
     let view (state: State) (dispatch) =
-        Button.create [
-            Button.top state.Y
-            Button.left state.X
-            Button.width 10.0
-            Button.height 10.0
-            Button.background "Blue"
-            Button.onPointerPressed (fun _ ->
-                Msg.Selected |> dispatch)
-            Button.onPointerReleased (fun _ ->
-                Msg.Deselected |> dispatch)
-            Button.onPointerMoved (fun e ->
-                let delta = e.GetPosition null
-                Msg.Move (delta.X, delta.Y) |> dispatch)
+        Canvas.create [
+            Canvas.name "DragArea"
+            Canvas.children [
+                for i in 0..state.ButtonLocations.Length - 1 do
+                    let button = state.ButtonLocations[i]
+                    Button.create [
+                        Button.top button.Y
+                        Button.left button.X
+                        Button.width 100.0
+                        Button.height 50.0
+                        Button.background "Blue"
+                        Button.onPointerPressed (fun e ->
+                            let newPointerLocation = e.GetPosition null
+                            Msg.Selected (i, newPointerLocation) |> dispatch)
+                        Button.onPointerReleased (fun _ ->
+                            Msg.Deselected |> dispatch)
+                        Button.onPointerMoved (fun e ->
+                            let newPointerLocation = e.GetPosition null
+                            Msg.Move (i, newPointerLocation) |> dispatch)
+                    ]
+            ]
         ]
 
 open Avalonia.Themes.Fluent
